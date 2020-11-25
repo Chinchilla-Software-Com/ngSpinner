baa5d9c8fba8e7927af43a4de8c75737bf8c54e6
Grover Jackson <grover@cdmdotnet.com>
2020-11-26 11:31:11 +1300

    Initial check-in

---
+++ src/spinner.js
@@ -0,0 +1,48 @@
+﻿angular.module("ngSpinner", []);
+
+angular
+	.module('ngSpinner')
+	.directive('loader', spinner);
+
+angular
+	.module('ngSpinner')
+	.directive('spinner', spinner);
+
+/* @ngInject */
+var spinner = function () {
+
+	return {
+		replace: false,
+		templateNamespace: 'svg',
+		scope: {
+			size: '@'
+		},
+		// NOTE: ng-attr- style binding is used to prevent SVG validation
+		// error messages.
+		template: [
+			'<svg class="circular" viewBox="25 25 50 50">',
+			'    <circle class="path" fill="none" stroke-width="2" stroke-miterlimit="10" cx="50" cy="50" r="20" />',
+			'</svg>',
+			'<div class="loader-progress"><span>{{ (progress == 0) ? "" : progress + " %" }}</span></div>'
+		].join(''),
+		restrict: 'CEA',
+		link: ["$scope", "el", "attrs", function ($scope, el, attrs) {
+			$scope.progress = 0;
+			$scope.$on("loader-progress-updated", function (newProgress) {
+				$scope.progress = newProgress;
+			});
+		}],
+		compile: function (tElement, tAttrs) {
+			// this is link function
+			return function (scope) {
+				scope.loaderName = tAttrs.loaderName;
+				scope.progress = 0;
+				scope.$on("loader-progress-updated", function (event, newValue) {
+					if (scope.loaderName == newValue.loaderName || newValue.loaderName == null || newValue.loaderName == "")
+						if (scope.progress != newValue.value)
+							scope.progress = newValue.value;
+				});
+			};
+		}
+	}
+};
