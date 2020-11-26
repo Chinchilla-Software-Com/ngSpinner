927fbd861d3210dea49415db3adb819ef7ff11af
Grover Jackson <grover@cdmdotnet.com>
2020-11-26 19:52:31 +1300

    Added missing styles and added support for a second style of loader/spinner.

--- src/spinner.js
+++ src/spinner.js
@@ -1,33 +1,44 @@
 ﻿var spinner = function () {
-
 	return {
 		replace: false,
-		templateNamespace: 'svg',
+		templateNamespace: "svg",
 		scope: {
-			size: '@'
+			spinnerStyle: "@",
+			spinnerName: "@"
 		},
 		// NOTE: ng-attr- style binding is used to prevent SVG validation
 		// error messages.
-		template: [
-			'<svg class="circular" viewBox="25 25 50 50">',
-			'    <circle class="path" fill="none" stroke-width="2" stroke-miterlimit="10" cx="50" cy="50" r="20" />',
-			'</svg>',
-			'<div class="loader-progress"><span>{{ (progress == 0) ? "" : progress + " %" }}</span></div>'
-		].join(''),
+		template: function (tElement, tAttrs) {
+			var progress = '<div class="spinner-progress"><span>{{ (progress == 0) ? "" : progress + " %" }}</span></div>';
+			var expando = ['<svg class="spinner-circular" viewBox="25 25 50 50">',
+				'<circle class="spinner-path" fill="none" stroke-width="2" stroke-miterlimit="10" cx="50" cy="50" r="20" />',
+				'</svg>',
+				progress
+			].join('');
+			var dotty = ['<div><div class="dotty"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div></div>',
+				progress
+			].join('');
+			switch (tAttrs.spinnerStyle) {
+				case "dotty":
+					return dotty;
+				default:
+					return expando;
+			}
+		},
 		restrict: 'CEA',
 		link: ["$scope", "el", "attrs", function ($scope, el, attrs) {
 			$scope.progress = 0;
-			$scope.$on("loader-progress-updated", function (newProgress) {
+			$scope.$on("spinner-progress-updated", function (newProgress) {
 				$scope.progress = newProgress;
 			});
 		}],
 		compile: function (tElement, tAttrs) {
 			// this is link function
 			return function (scope) {
-				scope.loaderName = tAttrs.loaderName;
+				scope.spinnerName = tAttrs.spinnerName;
 				scope.progress = 0;
-				scope.$on("loader-progress-updated", function (event, newValue) {
-					if (scope.loaderName == newValue.loaderName || newValue.loaderName == null || newValue.loaderName == "")
+				scope.$on("spinner-progress-updated", function (event, newValue) {
+					if (scope.spinnerName == newValue.spinnerName || newValue.spinnerName == null || newValue.spinnerName == "")
 						if (scope.progress != newValue.value)
 							scope.progress = newValue.value;
 				});
